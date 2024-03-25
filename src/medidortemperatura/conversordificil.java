c4734af23096cb891e4d3982b32d3dd7f30b0991
mateusfrazao1 <mateusfrazao12345@gmail.com>
2024-03-24 22:24:37 -0300

    Finalizado o segundo desafio, primeira vez utilizndo classes do pacote java.text e também do java.util

--- src/medidortemperatura/conversordificil.java
+++ src/medidortemperatura/conversordificil.java
@@ -1,4 +1,24 @@
 package medidortemperatura;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.text.DateFormat;
+
 public class conversordificil {
+    public static void main(String[] args) {
+        final double Multiplicador = 5 / 9.0;
+        final double Valor1 = 32;
+        //pegando as informações do usuario
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor em Fahrenhei:  ");
+        int Fahrenhei = scanner.nextInt();
+        //definindo a variavel Ceulsius
+        double Ceulsius = Fahrenhei - Valor1 * Multiplicador;
+        //Criando o objeto para Varriavel Ceulsius ter somente duas casas decimais
+        DecimalFormat df = new DecimalFormat("#.##");
+        //formatando a variavel Ceulsius
+        String ceulsius = df.format(Ceulsius);
+        System.out.println("Está fazendo atualmente: " + ceulsius + " Graus C");
+        scanner.close();
+    }
 }
